3fa7f9829038027cf4cacc3f984a9cd7041b21e3
FernandoTejedorL <fernandotejedorlopez@gmail.com>
2025-01-31 14:03:24 +0100

    all pages

---
+++ client/src/styles/colors.js
@@ -0,0 +1,11 @@
+export const COLORS = {
+	dark: '#302059',
+	deepViolet: '#B087E9',
+	lightViolet: '#CABBFF',
+	pink: '#FF8CB5',
+	saumon: '#F5D0C6',
+	turquoise: '#AEF8ED',
+	container: '#D9D2EF',
+	card: '#F4F1FF',
+	white: '#FFFFFF'
+};
