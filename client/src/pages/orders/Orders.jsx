6d132012068085cf722040b1e3aafa76b7f64269
FernandoTejedorL <fernandotejedorlopez@gmail.com>
2025-02-15 19:59:32 +0100

    orders recovered

--- client/src/pages/orders/Orders.jsx
+++ client/src/pages/orders/Orders.jsx
@@ -1,12 +1,23 @@
-import WorkInProgress from '../../components/workInProgress/WorkInProgress';
+import { useParams } from 'react-router-dom';
+import { findUserOrders } from '../../utils/api';
 
 const Orders = () => {
+	const { id } = useParams();
+	const userId = id;
+	const orders = ordersGot(userId);
+	console.log(orders);
+
 	return (
 		<div>
 			<h2>Orders Page</h2>
-			<WorkInProgress />
+			<h3>{id}</h3>
 		</div>
 	);
+};
+
+const ordersGot = async userId => {
+	const orders = await findUserOrders(userId);
+	console.log(orders);
 };
 
 export default Orders;
